5c42069a458081097dc41fa547b5f4ec8e5c5753
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-05-30 02:27:44 +0900

    Update App.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 function Modal(props){
   return(
     <div className='modal' style={{background : props.color}}>
-      <h4>{props.글제목}</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
       <button onClick={
@@ -25,14 +25,15 @@ function App() {
   let [글제목,글제목변경] = useState(['남자코트 추천', '강남 우동맛집','리액트 독학']);
   let [따봉,따봉변경] = useState([0,0,0]);
   let [modal,setModal] = useState(false);
+  let [title,setTitle] = useState(2)
   // map()사용법 
   // 1. array 자료 개수만큼 함수안의 코드 실행해줌
   // 2. 함수의 파라미터는 array안에 있던 자료 
   // 3. return에 뭐 적으면 array로 담아줌
-  [1,2,3].map(function(a){
-    // console.log(a)
-    return '1233211'
-  })
+  // [1,2,3].map(function(a){
+  //   // console.log(a)
+  //   return '1233211'
+  // })
   // function  함수(i){
   //   let copy = [...따봉];
   //   copy[i] = copy[i]+1;
@@ -84,7 +85,7 @@ function App() {
       {
         글제목.map(function(a,i){
           return(
-          <div className="list" key={i} >
+          <div className="list" key={i} onClick={()=>{setTitle(i)}}>
           <h4 onClick={()=>{setModal(modal == true? false : true)}}>{ 글제목[i] }
           <span onClick={()=>{
             let copy =[...따봉];
@@ -102,8 +103,10 @@ function App() {
       { } 안에 if문을 적을 수 없으니 삼항연산자를 이용
       */}
       {
-        modal == true? <Modal 글제목변경={글제목변경} color='yellow' 글제목={글제목} /> : null
+        modal == true? <Modal title={title} 글제목변경={글제목변경} color='yellow' 글제목={글제목} /> : null
       }
+      {/* 모달변수가 트루면 모달창을 실행
+      아니라면 null */}
       
     </div>
   );
